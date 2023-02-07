16d54e4d7bd58df26a1bf0f6198441db62752562
melineabrahamyan <abrahamyan.meline@gmail.com>
2023-02-07 22:11:30 +0400

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,53 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FlashcardsList from "./flashcardList";
 
 function App() {
-  const [flashcards, setFlashcards] = useState(FLASHCARDS);
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10").then((response) =>
+      response.json().then((result) => {
+        setFlashcards(
+          result.results.map((flashcard, index) => {
+            const answer = flashcard.correct_answer;
+            const options = [...flashcard.incorrect_answers, answer];
+            return {
+              id: `${index} - ${Date.now()}`,
+              question: flashcard.question,
+              answer: answer,
+              options: options.sort(() => 0.5 - Math.random()),
+            };
+          })
+        );
+      })
+    );
+  }, []);
+
   return (
     <div className="App">
       <FlashcardsList flashcards={flashcards} />
     </div>
   );
 }
-
-const FLASHCARDS = [
-  { id: 1, question: "What is 2+2", answer: 4, options: [1, 2, 4, 10] },
-  {
-    id: 2,
-    question: "What is the capital of France",
-    answer: "Paris",
-    options: ["Paris", "Moscow", "Berlin", "London"],
-  },
-  {
-    id: 2,
-    question: "What is the capital of France",
-    answer: "Paris",
-    options: ["Paris", "Moscow", "Berlin", "London"],
-  },
-  {
-    id: 2,
-    question: "What is the capital of France What is the capital of France",
-    answer: "Paris",
-    options: ["Paris", "Moscow", "Berlin", "London"],
-  },
-  {
-    id: 2,
-    question: "What is the capital of France",
-    answer: "Paris",
-    options: ["Paris", "Moscow", "Berlin", "London"],
-  },
-  {
-    id: 2,
-    question: "What is the capital of France",
-    answer: "Paris",
-    options: ["Paris", "Moscow", "Berlin", "London"],
-  },
-  {
-    id: 2,
-    question: "What is the capital of France",
-    answer: "Paris",
-    options: ["Paris", "Moscow", "Berlin", "London"],
-  },
-];
 
 export default App;
